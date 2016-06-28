78bf09dfc4fbc054586130cb88ca568df7200d4d
Allen McAfee <allen@robo3d.com>
2016-06-28 13:07:23 -0700

    Update Version.h

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -52,7 +52,7 @@
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
  */
-#define MACHINE_NAME "3D Printer"
+#define MACHINE_NAME "R2 Mini"
 
 /**
  * The SOURCE_CODE_URL is the location where users will find the Marlin Source
