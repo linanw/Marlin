f44e46dd01d4f0278df2c15f068b7c3a4ecbf419
Ximidar <ximidar@gmail.com>
2018-02-14 13:59:36 -0800

    Changed date to todays date with the version bump

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -49,7 +49,7 @@
    * here we define this default string as the date where the latest release
    * version was tagged.
    */
-  #define STRING_DISTRIBUTION_DATE "18 JAN 2018"
+  #define STRING_DISTRIBUTION_DATE "14 FEB 2018"
 
   /**
    * Required minimum Configuration.h and Configuration_adv.h file versions.
